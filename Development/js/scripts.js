c2460a3d175b7e5439df3d4b476cdf9be71f34a9
283-سيد حامد المهري <66621181+halmohri22@users.noreply.github.com>
2020-06-27 20:07:07 +0300

    تم العمل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,33 @@
 // السؤال الأول
 // 1
-
+const name= "حامد";
 // 2
-
+let age= 17;
 // 3 and 4
-
+console.log("انا"+name+"و عمري"+age); 
 // 5 and 6
-
+console.log (age*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobies= ["swimming","horse riding", "crossfit"];
 // 2
-
-// 3
-
+hobies.push=(age);
+console.log (hobies);
 // 4
-
+hobies.pop();
 // 5
-
+console.log (hobies);
 // 6
-
+let web = {
+    track: "web design",
+    code : "css", 
+    TA: ["moodhi","hussain", "rahaf", "nancy"],
+};
 // 7
-
+console.log (web.track);
+console.log (web.TA[0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
